90854624738e92ff13e18441f66ee9576d3626ed
yousunzoo <dpfvmffjqm@gmail.com>
2024-08-05 21:37:11 +0900

    chore: dotenv 설치

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    'nativewind/babel',
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env.local',
+        blocklist: null,
+        allowlist: null,
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
