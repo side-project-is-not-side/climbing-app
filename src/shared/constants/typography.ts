db5db517b71804d69209b77270fde682c6f98928
yousunzoo <dpfvmffjqm@gmail.com>
2024-08-11 19:19:50 +0900

    feat: typography 추가

---
+++ src/shared/constants/typography.ts
@@ -0,0 +1,40 @@
+const typography = {
+ '.font-display-1': {
+    fontWeight:'bold',
+    fontSize:24
+ },
+ '.font-header-1':{
+    fontWeight:'bold',
+    fontSize:18
+ },
+ '.font-header-2':{
+    fontWeight:'bold',
+    fontSize:16
+ },
+ '.font-header-3':{
+    fontWeight:'bold',
+    fontSize:14
+ },
+ '.font-text-1':{
+    fontWeight:'regular',
+    fontSize:16
+ },
+ '.font-text-2':{
+    fontWeight:'regular',
+    fontSize:14
+ },
+ '.font-flag':{
+    fontWeight:'regular',
+    fontSize:12
+ },
+ '.font-bottom-tab-active':{
+    fontWeight:'bold',
+    fontSize:10
+ },
+ '.font-bottom-tab-inactive':{
+    fontWeight:'regular',
+    fontSize:10
+ }
+};
+
+export default typography;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,42 @@
+const typography = {
+  '.font-display-1': {
+     fontWeight:'bold',
+     fontSize:24
+  },
+  '.font-header-1':{
+     fontWeight:'bold',
+     fontSize:18
+  },
+  '.font-header-2':{
+     fontWeight:'bold',
+     fontSize:16
+  },
+  '.font-header-3':{
+     fontWeight:'bold',
+     fontSize:14
+  },
+  '.font-text-1':{
+     fontWeight:'regular',
+     fontSize:16
+  },
+  '.font-text-2':{
+     fontWeight:'regular',
+     fontSize:14
+  },
+  '.font-flag':{
+     fontWeight:'regular',
+     fontSize:12
+  },
+  '.font-bottom-tab-active':{
+     fontWeight:'bold',
+     fontSize:10
+  },
+  '.font-bottom-tab-inactive':{
+     fontWeight:'regular',
+     fontSize:10
+  }
+ };
+ 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './App.{js,jsx,ts,tsx}'],
@@ -48,6 +87,9 @@ module.exports = {
         notoSans: ['NotoSansKR'],
       },
     },
-  },
-  plugins: [],
+  }, plugins: [
+    ({ addUtilities }) => {
+      addUtilities(typography);
+    },
+  ]
 };
