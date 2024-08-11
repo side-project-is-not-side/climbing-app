ef1be30b22da576108b8e00b2f44dee6eb0e4ae1
yousunzoo <dpfvmffjqm@gmail.com>
2024-08-11 22:29:04 +0900

    refactor: 특정 route에서만 touchable 처리

--- src/entities/map/ui/CurrentGymCard.tsx
+++ src/entities/map/ui/CurrentGymCard.tsx
@@ -1,35 +1,45 @@
+import { GetGymDetailResponse } from '@entities/gym/api/types';
 import { useGetGymDetailInfo } from '@entities/gym/queries';
 import Chips from '@entities/gym/ui/Chips';
-import { useNavigation } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { MAP_ROUTES, MapRoute } from '@shared/constants';
 import { LoadingSpinner } from '@shared/ui';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 
 
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 
-function CurrentGymCard({id}:{id:number}) {
+const Wrapper = ({children, id}:PropsWithChildren<{id:number}>) => {
+  const route = useRoute<RouteProp<MapRoute>>();
   const navigation = useNavigation<NativeStackNavigationProp<MapRoute>>();
-
-
-  const { isLoading, data } = useGetGymDetailInfo(id);
-
-  if (isLoading || !data) return <LoadingSpinner />
-
-  const {thumbnailImageUrl,name,distance,roadNameAddress,tags} = data;
-
   const onPress = () => {
     navigation.navigate(MAP_ROUTES.DETAIL, {id})
   }
+
+  if(route.name === 'nearby') {
+    return <TouchableOpacity onPress={onPress}>{children}</TouchableOpacity>
+  }
+  return children;
+}
+
+function CurrentGymCard({
+  id,
+  name,
+  thumbnailImageUrl,
+  distance,
+  roadNameAddress,
+  tags,
+}: Pick<GetGymDetailResponse,'id' | 'name' | 'thumbnailImageUrl' | 'distance' | 'roadNameAddress' | 'tags'>) {
+
   return (
     <View>
       <Text className="font-header-1 text-white mb-5">
         암장 정보
       </Text>
 
-      <TouchableOpacity onPress={onPress}>
+      <Wrapper id={id}>
         <View className="relative overflow-hidden w-full h-[156px] rounded-[10px] mb-5">
           <Image source={{uri:thumbnailImageUrl}} alt={name} height={156} />
         </View>
@@ -51,7 +61,7 @@ function CurrentGymCard({id}:{id:number}) {
             <Chips chips={tags} />
           </View>
         </View>
-      </TouchableOpacity>
+      </Wrapper>
     </View>
   );
 }
