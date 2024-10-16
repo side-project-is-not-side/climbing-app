06289da7ea7c2c4e3e945e44b55bf0cd6479d850
seunghye <tmdgp0212@naver.com>
2024-10-16 13:26:36 +0900

    feat: 카카오 앱 로그인 설정

--- src/app/navigation/AuthNavigation.tsx
+++ src/app/navigation/AuthNavigation.tsx
@@ -1,27 +1,23 @@
-import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
 import {LoginScreen, OnboardingScreen} from '../../screens';
-
 import {AUTH_ROUTES, AuthRoute} from '../../shared/constants/routes';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {colors} from '@shared/constants';
+import React from 'react';
 import {SafeAreaView} from 'react-native';
 
 const Stack = createNativeStackNavigator<AuthRoute>();
 
-const AuthNavigation = ({
-  initialRouteName,
-}: {
-  initialRouteName: keyof AuthRoute | undefined;
-}) => {
+const AuthNavigation = ({initialRouteName}: {initialRouteName: keyof AuthRoute | undefined}) => {
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#000'}}>
       <Stack.Navigator
-        screenOptions={{headerShown: false}}
+        screenOptions={{
+          headerShown: false,
+          headerStyle: {backgroundColor: colors.gray800},
+          contentStyle: {backgroundColor: colors.gray800},
+        }}
         initialRouteName={initialRouteName}>
-        <Stack.Screen
-          name={AUTH_ROUTES.ONBOARDING}
-          component={OnboardingScreen}
-        />
+        <Stack.Screen name={AUTH_ROUTES.ONBOARDING} component={OnboardingScreen} />
         <Stack.Screen name={AUTH_ROUTES.LOGIN} component={LoginScreen} />
       </Stack.Navigator>
     </SafeAreaView>
