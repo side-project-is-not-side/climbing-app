679c95be72993e2c5320f4c0d979f260ef6f2b84
yousunzoo <dpfvmffjqm@gmail.com>
2024-10-20 20:55:53 +0900

    feat: 지도에서 재검색 버튼 추가

--- src/app/navigation/MyNavigation.tsx
+++ src/app/navigation/MyNavigation.tsx
@@ -26,7 +26,7 @@ const MyNavigation = () => {
           },
           headerLeft: () => (
             <Pressable onPress={navigation.goBack} className="items-center justify-center w-6 h-6">
-              <Icon name="ChevronLeft" size={14} />
+              <Icon name="ArrowLeft" size={14} />
             </Pressable>
           ),
           animation: 'fade_from_bottom',
--- src/entities/map/ui/NearbyMap.tsx
+++ src/entities/map/ui/NearbyMap.tsx
@@ -4,7 +4,9 @@ import Marker from './Marker';
 import {AroundGym} from '@entities/gym/api/types';
 import {useGetNearbyGyms} from '@entities/gym/queries';
 import {NaverMapView, NaverMapViewRef} from '@mj-studio/react-native-naver-map';
-import React, {useEffect, useRef} from 'react';
+import {Icon} from '@shared/ui';
+import React, {useEffect, useRef, useState} from 'react';
+import {Pressable, Text, View} from 'react-native';
 
 type NearbyMapProps = {
   selected?: number;
@@ -13,7 +15,8 @@ type NearbyMapProps = {
 const NearbyMap = ({selected, setSelected}: NearbyMapProps) => {
   const {currentLocation, bounds, onCameraChanged, granted} = useCurrentLocation(DEFAULT_ZOOM);
   const ref = useRef<NaverMapViewRef>(null);
-  const {data} = useGetNearbyGyms(bounds);
+  const [currentBounds, setCurrentBounds] = useState(bounds);
+  const {data} = useGetNearbyGyms(currentBounds);
 
   const onMarkerTap =
     ({id, latitude, longitude}: AroundGym) =>
@@ -41,25 +44,39 @@ const NearbyMap = ({selected, setSelected}: NearbyMapProps) => {
     ref.current?.setLocationTrackingMode('NoFollow');
   }, [granted]);
 
+  const handlePress = () => {
+    setCurrentBounds(bounds);
+  };
+
   return (
-    <NaverMapView
-      ref={ref}
-      mapType="Basic"
-      style={{flex: 1}}
-      onCameraChanged={onCameraChanged}
-      // initialRegion={currentLocation}
-      isShowZoomControls={false}
-      initialCamera={{...currentLocation, zoom: DEFAULT_ZOOM}}>
-      {data?.map(({id, latitude, longitude}) => (
-        <Marker
-          key={id}
-          latitude={latitude}
-          longitude={longitude}
-          type={selected === id ? 'active' : 'inactive'}
-          onTap={onMarkerTap({id, latitude, longitude})}
-        />
-      ))}
-    </NaverMapView>
+    <View className="relative w-full h-full">
+      <NaverMapView
+        className="top-0"
+        ref={ref}
+        mapType="Basic"
+        style={{flex: 1}}
+        onCameraChanged={onCameraChanged}
+        // initialRegion={currentLocation}
+        isShowZoomControls={false}
+        initialCamera={{...currentLocation, zoom: DEFAULT_ZOOM}}>
+        <Pressable
+          className="top-[23px] self-center z-10 flex flex-row gap-x-0.5 items-center justify-center w-[188px] bg-neutral-white rounded-[100px] py-[14px] px-10 shadow-md"
+          onPress={handlePress}>
+          <Icon className="z-10" name="Redo" size={16} />
+          <Text className="z-10 font-text-2">지도에서 재검색</Text>
+        </Pressable>
+
+        {data?.map(({id, latitude, longitude}) => (
+          <Marker
+            key={id}
+            latitude={latitude}
+            longitude={longitude}
+            type={selected === id ? 'active' : 'inactive'}
+            onTap={onMarkerTap({id, latitude, longitude})}
+          />
+        ))}
+      </NaverMapView>
+    </View>
   );
 };
 
