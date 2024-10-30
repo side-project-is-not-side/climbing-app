b1d25b97aa770b518fed97dbd7b3625024d4435a
yousunzoo <dpfvmffjqm@gmail.com>
2024-10-30 20:34:49 +0900

    fix: kakaoSDK build error 수정

--- ios/grabbers/AppDelegate.mm
+++ ios/grabbers/AppDelegate.mm
@@ -2,7 +2,7 @@
 
 #import "RNSplashScreen.h"
 #import <React/RCTBundleURLProvider.h>
-#import <KakaoOpenSDK/KakaoOpenSDK.h>
+#import <RNKakaoLogins.h>
 #import <UIKit/UIKit.h>
 
 @implementation AppDelegate
@@ -25,13 +25,13 @@
 }
 
 - (BOOL)application:(UIApplication *)app
-        openURL:(NSURL *)url
-        options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-  if ([KOSession isKakaoAccountLoginCallback:url]) {
-    return [KOSession handleOpenURL:url];
-  }
-  
-  return NO;
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
+
+ return NO;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
