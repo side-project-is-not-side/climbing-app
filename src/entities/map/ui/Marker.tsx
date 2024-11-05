e32c2d2484636f744f61f1dda34ba7cc7b896f06
yousunzoo <dpfvmffjqm@gmail.com>
2024-11-06 00:38:55 +0900

    refactor: 마커 원래대로 원복

--- src/entities/map/ui/Marker.tsx
+++ src/entities/map/ui/Marker.tsx
@@ -3,7 +3,6 @@ import {IconName} from '../../../shared/ui/Icon/Icon';
 import {MarkerType} from '../types';
 import {NaverMapMarkerOverlay, NaverMapMarkerOverlayProps} from '@mj-studio/react-native-naver-map';
 import React from 'react';
-import {View} from 'react-native';
 
 type MarkerProps = {type?: MarkerType} & Omit<NaverMapMarkerOverlayProps, 'width' | 'height'>;
 
@@ -17,8 +16,7 @@ const markerIconName: Record<MarkerType, IconName> = {
 const Marker = ({type = 'inactive', ...props}: MarkerProps) => {
   return (
     <NaverMapMarkerOverlay width={30} height={30} {...props}>
-      {/* <Icon name={markerIconName[type]} size={30} /> */}
-      <View style={{width: 30, height: 30, backgroundColor: 'red'}} />
+      <Icon name={markerIconName[type]} size={30} />
     </NaverMapMarkerOverlay>
   );
 };
