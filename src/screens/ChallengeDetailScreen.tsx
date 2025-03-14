355b816fa29f109285a0bbb2e41ab4d783133b39
yousunzoo <dpfvmffjqm@gmail.com>
2025-03-14 23:34:43 +0900

    feat: 챌린지 상세 화면에 출석 챌린지 조건 추가 및 관련 컴포넌트 수정

--- src/screens/ChallengeDetailScreen.tsx
+++ src/screens/ChallengeDetailScreen.tsx
@@ -1,8 +1,19 @@
 import {ChallengeDetail} from '../widgets';
+import {RouteProp, useRoute} from '@react-navigation/native';
+import {ChallengeRoute} from '@shared/constants';
+import {AttendanceChallenge} from '@widgets/challenge';
 import React from 'react';
 
 const ChallengeDetailScreen = () => {
-  return <ChallengeDetail />;
+  const route = useRoute<RouteProp<ChallengeRoute, 'challenge_detail'>>();
+
+  const {activityType, challengeId} = route.params;
+
+  if (activityType === 'ATTENDANCE') {
+    return <AttendanceChallenge challengeId={challengeId} activityType={activityType} />;
+  }
+
+  return <ChallengeDetail challengeId={challengeId} activityType={activityType} />;
 };
 
 export default ChallengeDetailScreen;
